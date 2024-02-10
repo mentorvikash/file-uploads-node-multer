91465a5d27104e02de2eace841eb81dcb9241cb5
mentorvikash <mentorvikash@gmail.com>
2024-02-11 01:38:38 +0530

    created route to uplaod file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,18 @@
 const express = require("express")
+const path = require("path") // path module provided by node
 const routes = express.Router()
+const multer = require("multer")
 
+const upload = multer({ storage });
+
+// Home page route
 routes.get("/", (req, res) => {
-    res.send("welcome to home page")
+    res.render("index")
 })
 
-
-
+// api to upload files
+routes.post("/upload", upload.single("file"), (req, res) => {
+    res.redirect("/")
+})
 
 module.exports = routes
